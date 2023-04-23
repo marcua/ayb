0ae77c79d444ecac29ee5140421e36f750151f5f
Adam Marcus <marcua@marcua.net>
2023-04-23 16:26:21 +0000

    Add data path to server config

--- src/hosted_db/paths.rs
+++ src/hosted_db/paths.rs
@@ -2,10 +2,12 @@ use crate::error::AybError;
 use std::fs;
 use std::path::PathBuf;
 
-pub fn database_path(entity_slug: &str, database_slug: &str) -> Result<PathBuf, AybError> {
-    // TODO(marcua): make the path relate to some
-    // persistent storage (with high availability, etc.)
-    let mut path: PathBuf = ["/tmp", "ayb", entity_slug].iter().collect();
+pub fn database_path(
+    entity_slug: &str,
+    database_slug: &str,
+    data_path: &str,
+) -> Result<PathBuf, AybError> {
+    let mut path: PathBuf = [data_path, entity_slug].iter().collect();
     if let Err(e) = fs::create_dir_all(&path) {
         return Err(AybError {
             message: format!("Unable to create entity path for {}: {}", entity_slug, e),
