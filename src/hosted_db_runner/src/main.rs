5eb2038f29b888b63bddb86d7e1702d9982acf9c
Adam Marcus <marcua@marcua.net>
2023-11-21 03:11:12 +0000

    A version that won't build

---
+++ src/hosted_db_runner/src/main.rs
@@ -0,0 +1,58 @@
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use std::vec::Vec;
+//use rusqlite;
+//use rusqlite::types::ValueRef;
+
+use sqlx::{
+    migrate,
+    postgres::PgPoolOptions,
+    sqlite::{SqliteConnectOptions, SqlitePoolOptions},
+    Pool, Postgres, Sqlite,
+};
+
+
+
+#[derive(Serialize, Debug, Deserialize)]
+pub struct QueryResult {
+    pub fields: Vec<String>,
+    pub rows: Vec<Vec<Option<String>>>,
+}
+
+pub fn run_sqlite_query(path: &PathBuf, query: &str) -> Result<QueryResult, std::io::Error> {
+    let conn = rusqlite::Connection::open(path)?;
+    let mut prepared = conn.prepare(query)?;
+    let num_columns = prepared.column_count();
+    let mut fields: Vec<String> = Vec::new();
+    for column_index in 0..num_columns {
+        fields.push(String::from(prepared.column_name(column_index)?))
+    }
+
+    let mut rows = prepared.query([])?;
+    let mut results: Vec<Vec<Option<String>>> = Vec::new();
+    while let Some(row) = rows.next()? {
+        let mut result: Vec<Option<String>> = Vec::new();
+        for column_index in 0..num_columns {
+            let column_value = row.get_ref(column_index)?;
+            result.push(match column_value {
+                ValueRef::Null => None,
+                ValueRef::Integer(i) => Some(i.to_string()),
+                ValueRef::Real(f) => Some(f.to_string()),
+                ValueRef::Text(_t) => Some(column_value.as_str()?.to_string()),
+                ValueRef::Blob(_b) => {
+                    Some(std::str::from_utf8(column_value.as_bytes()?)?.to_string())
+                }
+            });
+        }
+        results.push(result);
+    }
+    Ok(QueryResult {
+        fields,
+        rows: results,
+    })
+}
+
+
+fn main() -> std::io::Result<()> {    
+    println!("{:#?}", run_sqlite_query(&PathBuf::from("testing.sqlite"), "CREATE TABLE TABLE moo (hello varchar(20));")?);
+}
