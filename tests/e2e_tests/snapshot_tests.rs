e7e57b2afadc0767c24a0f69f557bb04e00f22d8
Adam Marcus <marcua@marcua.net>
2024-06-09 13:29:39 +0000

    Add snapshot tests

---
+++ tests/e2e_tests/snapshot_tests.rs
@@ -0,0 +1,80 @@
+use crate::e2e_tests::{
+    FIRST_ENTITY_DB, FIRST_ENTITY_DB_CASED, FIRST_ENTITY_DB_SLUG, FIRST_ENTITY_SLUG,
+};
+use crate::utils::ayb::{list_snapshots, query};
+use crate::utils::testing::snapshot_storage;
+use std::collections::HashMap;
+use std::thread;
+use std::time;
+
+pub async fn test_snapshots(
+    db_type: &str,
+    config_path: &str,
+    api_keys: &HashMap<String, Vec<String>>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let snapshots = snapshot_storage(db_type).await?;
+    snapshots
+        .dangerously_delete_prefix(FIRST_ENTITY_SLUG, FIRST_ENTITY_DB_SLUG)
+        .await?;
+
+    // Can't list snapshots from an account without access.
+    list_snapshots(
+        &config_path,
+        &api_keys.get("second").unwrap()[0],
+        FIRST_ENTITY_DB,
+        "csv",
+        "Error: Authenticated entity e2e-second can not manage snapshots on database e2e-first/test.sqlite",
+    )?;
+
+    // Can list snapshots from the first set of API keys.
+    list_snapshots(
+        &config_path,
+        &api_keys.get("first").unwrap()[0],
+        FIRST_ENTITY_DB_CASED,
+        "csv",
+        "No snapshots for E2E-FiRST/test.sqlite",
+    )?;
+
+    // We'll sleep between various checks in this test to allow the
+    // snapshotting logic, which runs every 2 seconds, to execute.
+    thread::sleep(time::Duration::from_secs(3));
+    list_snapshots(
+        &config_path,
+        &api_keys.get("first").unwrap()[0],
+        FIRST_ENTITY_DB,
+        "csv",
+        r"Name,Last modified\nbucket/sqlite/e2e-first/test.sqlite/notimplemented,\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC",
+    )?;
+
+    // No change to database, so same number of snapshots after sleep.
+    thread::sleep(time::Duration::from_secs(3));
+    list_snapshots(
+        &config_path,
+        &api_keys.get("first").unwrap()[0],
+        FIRST_ENTITY_DB,
+        "csv",
+        r"Name,Last modified\nbucket/sqlite/e2e-first/test.sqlite/notimplemented,\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC",
+    )?;
+
+    // Modify database, wait, and ensure a new snapshot was taken.
+    query(
+        &config_path,
+        &api_keys.get("first").unwrap()[1],
+        "INSERT INTO test_table (fname, lname) VALUES (\"another first\", \"another last\");",
+        FIRST_ENTITY_DB,
+        "table",
+        "\nRows: 0",
+    )?;
+    thread::sleep(time::Duration::from_secs(3));
+    // TODO(marcua): When we store multiple snapshots with hashing,
+    // there should be two snapshots instead of one here.
+    list_snapshots(
+        &config_path,
+        &api_keys.get("first").unwrap()[0],
+        FIRST_ENTITY_DB,
+        "csv",
+        r"Name,Last modified\nbucket/sqlite/e2e-first/test.sqlite/notimplemented,\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} UTC",
+    )?;
+
+    Ok(())
+}
