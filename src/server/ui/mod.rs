0f9ad81694cb11159c6a05b9777b762b8a457165
Adam Marcus <marcua@marcua.net>
2025-02-08 12:35:09 -0500

    refactor: Remove duplicate module and route declarations

--- src/server/ui/mod.rs
+++ src/server/ui/mod.rs
@@ -1,9 +1,3 @@
 mod display_user;
 
 pub use display_user::display_user as display_user_route;
-mod display_user;
-
-pub use display_user::display_user as display_user_route;
-mod display_user;
-
-pub use display_user::display_user as display_user_route;
